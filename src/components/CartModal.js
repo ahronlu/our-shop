717b53f39e7abb1cfa33abe36859cc3cbe3f0514
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-09-13 22:10:16 +0300

    Update CartModal.js

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Modal, Button, Col, Row } from "react-bootstrap" ;
-import { useSelector } from "react-redux";
 
 export const CartModal = () => {
     const [show, setShow] = useState(false);
