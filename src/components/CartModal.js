c98ab4328e605de4924945aaa63d4389fcea98bd
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-09-13 22:11:47 +0300

    Update CartModal.js

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -17,19 +17,19 @@ export const CartModal = () => {
 
             <Modal.Body>
               {cartItems.length ? (
-        cartItems?.map((item) => (
-          <Row className="align-items-center mb-5">
-            <Col xs={6} md={2}>
-              <img src={item.image} alt={item.title} height="100" />
-            </Col>
-            <Col xs={6} md={2}>
-              <h2>{item.title}</h2>
-            </Col>
-          </Row>
-        ))
-      ) : (
-        <h2>Your cart is empty</h2>
-      )}
+               cartItems?.map((item) => (
+                <Row className="align-items-center mb-5">
+                  <Col xs={6} md={2}>
+                    <img src={item.image} alt={item.title} height="100" />
+                  </Col>
+                 <Col xs={6} md={2}>
+                   <h2>{item.title}</h2>
+                 </Col>
+                </Row>
+               ))
+             ) : (
+              <h2>Your cart is empty</h2>
+             )}
             </Modal.Body>
 
             <Modal.Footer>
