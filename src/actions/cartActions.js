2a2cac909f1366af8602a9bc18fa538b28ab9d8f
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-09-14 00:15:04 +0300

    Update cartActions.js

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -2,7 +2,27 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_INCREMENT_ITEM,
+  CART_DECREMENT_ITEM
 } from "../constants/cartConstants";
+
+export const incrementCartItem = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_INCREMENT_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const decrementCartItem = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_DECREMENT_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
 
 export const addToCart = (product) => async (dispatch, getState) => {
   dispatch({
