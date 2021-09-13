10d871ceb0b3a0adf2e19799cb5bd3c8ca4f050c
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-09-13 22:04:54 +0300

    Update CartModal.js

--- src/components/CartModal.js
+++ src/components/CartModal.js
@@ -1,25 +1,29 @@
 import { useState } from "react";
-import { Modal } from "react-bootstrap" ;
+import { useSelector, useDispatch } from "react-redux";
+import { Modal, Button } from "react-bootstrap" ;
+import { useSelector } from "react-redux";
 
 export const CartModal = () => {
     const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+    const { cartItems } = useSelector((state) => state.cart);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
   
-return (
-      <Modal show={show} onHide={handleClose}>
-    <Modal.Header closeButton>
-    </Modal.Header>
+    return (
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            </Modal.Header>
 
-    <Modal.Body>
-      <p>Modal body text goes here.</p>
-    </Modal.Body>
+            <Modal.Body>
+              <p>Modal body text goes here.</p>
+            </Modal.Body>
 
-    <Modal.Footer>
-      <p><b>Total:</b> 26.98%</p>
-      <Button variant="primary">Checkout</Button>
-    </Modal.Footer>
-  </Modal>
+            <Modal.Footer>
+              <p><b>Total:</b> 26.98%</p>
+              <Button variant="primary">Checkout</Button>
+            </Modal.Footer>
+          </Modal>
   )
 }
