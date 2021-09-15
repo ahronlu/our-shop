15469b1bf72370b27ecf40c896d31b63e9cd9328
koftov <ahronlu@gmail.com>
2021-09-15 09:07:04 +0300

    add alerts to paypal

--- src/components/PaypalButton.js
+++ src/components/PaypalButton.js
@@ -6,6 +6,7 @@ export default class PaypalButton extends React.Component {
     const onSuccess = (payment) => {
       // Congratulation, it came here means everything's fine!
       console.log("The payment was succeeded!", payment);
+      alert("The payment was succeed!");
       this.props.clearCart();
       this.props.history.push("/");
       // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
@@ -14,6 +15,7 @@ export default class PaypalButton extends React.Component {
     const onCancel = (data) => {
       // User pressed "cancel" or close Paypal's popup!
       console.log("The payment was cancelled!", data);
+      alert("The payment was cancelled!");
       // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
     };
 
